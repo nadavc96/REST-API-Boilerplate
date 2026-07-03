d04c3aa4ab42410357137a66b8234aa8ed596fe8
nadav <nadavx96@walla.com>
2026-07-03 12:25:56 +0300

    refactor auth tests with helper functions

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -2,11 +2,30 @@ import request from "supertest";
 import app from "../app";
 import { pool } from "../db/pool";
 
+const testUser = {
+  email: "test@test.com",
+  password: "password123",
+};
+
+const register = async () => {
+  return await request(app).post("/auth/register").send(testUser);
+};
+
+const login = async () => {
+  return await request(app).post("/auth/login").send(testUser);
+};
+
+const registerAndLoginWithAgent = async () => {
+  const agent = request.agent(app);
+  await agent.post("/auth/register").send(testUser);
+  const loginRes = await agent.post("/auth/login").send(testUser);
+
+  return { agent, accessToken: loginRes.body.accessToken };
+};
+
 describe("POST /auth/register", () => {
   it("should register a new user and return 201", async () => {
-    const res = await request(app)
-      .post("/auth/register")
-      .send({ email: "test@test.com", password: "password123" });
+    const res = await register();
 
     expect(res.statusCode).toBe(201);
     expect(res.body.message).toBe("Account created successfully");
@@ -14,15 +33,10 @@ describe("POST /auth/register", () => {
 
   it("should return error if email already in use", async () => {
     //register first
-    await request(app).post("/auth/register").send({
-      email: "test@test.com",
-      password: "password123",
-    });
+    await register();
 
     //register with the same email
-    const res = await request(app)
-      .post("/auth/register")
-      .send({ email: "test@test.com", password: "password123" });
+    const res = await register();
 
     expect(res.statusCode).toBe(409);
     expect(res.body.error).toBe("Email already in use");
@@ -31,16 +45,17 @@ describe("POST /auth/register", () => {
   it("should return 400 if email is invalid", async () => {
     const res = await request(app)
       .post("/auth/register")
-      .send({ email: "notemail", password: "password123" });
+      .send({ email: "notemail", password: testUser.password });
 
     expect(res.statusCode).toBe(400);
     expect(res.body.error[0].message).toBe("Invalid email address");
   });
 
   it("should return 400 if password is too short", async () => {
+    await register();
     const res = await request(app)
       .post("/auth/register")
-      .send({ email: "test@test.com", password: "123" });
+      .send({ email: testUser.email, password: "123" });
 
     expect(res.statusCode).toBe(400);
     expect(res.body.error[0].message).toBe(
@@ -51,14 +66,8 @@ describe("POST /auth/register", () => {
 
 describe("POST /auth/login", () => {
   it("should login a user and return 200", async () => {
-    await request(app).post("/auth/register").send({
-      email: "test@test.com",
-      password: "password123",
-    });
-
-    const res = await request(app)
-      .post("/auth/login")
-      .send({ email: "test@test.com", password: "password123" });
+    await register();
+    const res = await login();
 
     expect(res.statusCode).toBe(200);
     expect(res.body.accessToken).toBeDefined();
@@ -66,118 +75,90 @@ describe("POST /auth/login", () => {
   });
 
   it("should return 401 if password is wrong", async () => {
-    await request(app)
-      .post("/auth/register")
-      .send({ email: "test@test.com", password: "password123" });
-
+    await register();
     const res = await request(app)
       .post("/auth/login")
-      .send({ email: "test@test.com", password: "password125" });
+      .send({ email: testUser.email, password: "password1234" });
 
     expect(res.statusCode).toBe(401);
     expect(res.body.error).toBe("Email or password is wrong");
   });
 
   it("should return 401 if email doesnt exist", async () => {
-    const res = await request(app)
-      .post("/auth/login")
-      .send({ email: "test@test.com", password: "password123" });
+    const res = await login();
 
     expect(res.statusCode).toBe(401);
     expect(res.body.error).toBe("Email or password is wrong");
   });
+});
 
-  describe("POST /auth/refresh", () => {
-    it("should return 200 and a new access token", async () => {
-      const agent = request.agent(app);
+describe("POST /auth/refresh", () => {
+  it("should return 200 and a new access token", async () => {
+    const { agent } = await registerAndLoginWithAgent();
 
-      await agent
-        .post("/auth/register")
-        .send({ email: "test@test.com", password: "password123" });
-      await agent
-        .post("/auth/login")
-        .send({ email: "test@test.com", password: "password123" });
+    const res = await agent.post("/auth/refresh");
 
-      const res = await agent.post("/auth/refresh");
-
-      expect(res.statusCode).toBe(200);
-      expect(res.body.accessToken).toBeDefined();
-      expect(typeof res.body.accessToken).toBe("string");
-    });
-
-    it("should return 401 if there is no cookie", async () => {
-      const res = await request(app).post("/auth/refresh");
-
-      expect(res.statusCode).toBe(401);
-      expect(res.body.error).toBe("No refresh token provided");
-    });
-
-    it("should return 401 if token is invalid", async () => {
-      const res = await request(app)
-        .post("/auth/refresh")
-        .set("Cookie", "refreshToken=invalidtoken");
-
-      expect(res.statusCode).toBe(401);
-      expect(res.body.error).toBe("Invalid refresh token.");
-    });
-
-    it("should return 401 if token is expired", async () => {
-      const agent = request.agent(app);
-
-      await agent
-        .post("/auth/register")
-        .send({ email: "test@test.com", password: "password123" });
-      await agent
-        .post("/auth/login")
-        .send({ email: "test@test.com", password: "password123" });
-
-      await pool.query(
-        `UPDATE refresh_tokens SET expires_at = NOW() - INTERVAL '1 day'`,
-      );
-      const res = await agent.post("/auth/refresh");
-
-      expect(res.statusCode).toBe(401);
-      expect(res.body.error).toBe("Refresh token expired");
-    });
+    expect(res.statusCode).toBe(200);
+    expect(res.body.accessToken).toBeDefined();
+    expect(typeof res.body.accessToken).toBe("string");
   });
 
-  describe("POST /auth/logout", () => {
-    it("should return 200 if user logged out", async () => {
-      const agent = request.agent(app);
+  it("should return 401 if there is no cookie", async () => {
+    const res = await request(app).post("/auth/refresh");
 
-      await agent
-        .post("/auth/register")
-        .send({ email: "test@test.com", password: "password123" });
-      const loginRes = await agent
-        .post("/auth/login")
-        .send({ email: "test@test.com", password: "password123" });
+    expect(res.statusCode).toBe(401);
+    expect(res.body.error).toBe("No refresh token provided");
+  });
 
-      const accessToken = loginRes.body.accessToken;
+  it("should return 401 if token is invalid", async () => {
+    const res = await request(app)
+      .post("/auth/refresh")
+      .set("Cookie", "refreshToken=invalidtoken");
 
-      const res = await agent
-        .post("/auth/logout")
-        .set("Authorization", `Bearer ${accessToken}`);
+    expect(res.statusCode).toBe(401);
+    expect(res.body.error).toBe("Invalid refresh token.");
+  });
 
-      expect(res.statusCode).toBe(200);
-      expect(res.body.message).toBe("Logged out successfully");
-    });
+  it("should return 401 if token is expired", async () => {
+    const { agent } = await registerAndLoginWithAgent();
 
-    it("should return 401 if no cookie", async () => {
-      const res = await request(app)
-        .post("/auth/logout")
-        .set("Authorization", "Bearer faketoken");
+    await pool.query(
+      `UPDATE refresh_tokens SET expires_at = NOW() - INTERVAL '1 day'`,
+    );
+    const res = await agent.post("/auth/refresh");
 
-      expect(res.statusCode).toBe(401);
-      expect(res.body.error).toBe("Invalid or expired token");
-    });
+    expect(res.statusCode).toBe(401);
+    expect(res.body.error).toBe("Refresh token expired");
+  });
+});
 
-    it("should return 401 if no access token", async () => {
-      const res = await request(app)
-        .post("/auth/logout")
-        .set("Cookie", "refreshToken=sometoken");
+describe("POST /auth/logout", () => {
+  it("should return 200 if user logged out", async () => {
+    const { agent, accessToken } = await registerAndLoginWithAgent();
 
-      expect(res.statusCode).toBe(401);
-      expect(res.body.error).toBe("No token provided");
-    });
+    const res = await agent
+      .post("/auth/logout")
+      .set("Authorization", `Bearer ${accessToken}`);
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body.message).toBe("Logged out successfully");
+  });
+
+  it("should return 401 if no cookie", async () => {
+    const res = await request(app)
+      .post("/auth/logout")
+      .set("Authorization", "Bearer faketoken");
+
+    expect(res.statusCode).toBe(401);
+    expect(res.body.error).toBe("Invalid or expired token");
+  });
+
+  it("should return 401 if no access token", async () => {
+    const res = await request(app)
+      .post("/auth/logout")
+      .set("Cookie", "refreshToken=sometoken");
+
+    expect(res.statusCode).toBe(401);
+    expect(res.body.error).toBe("No token provided");
   });
 });
